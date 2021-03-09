c75c0b9a037aa2baa50ec6d54c142cc424b833d5
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-03-10 01:37:49 +0330

    d

--- integration.tf
+++ integration.tf
@@ -17,7 +17,7 @@ resource "kubernetes_pod" "integration" {
     container {
       name    = "cypress"
       image   = "hsndocker/integration-cypress:latest"
-      # command = ["/bin/bash", "-c", "sleep infinity"]
+      command = ["/bin/bash", "-c", "sleep infinity"]
 
       resources {
         limits = {
